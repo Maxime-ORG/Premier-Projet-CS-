3a433346d003efe88f912db6e3348db5c85b2bb1
Maxime-ORG <143593084+Maxime-ORG@users.noreply.github.com>
2023-12-21 16:54:14 +0100

    Add files via upload

---
+++ Helloworld/Message.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Helloworld
+{
+    internal class Message
+    {
+        
+        private String _message;
+        private double _hMorning;
+        private double _hMidday;
+        private double _hAfternoon;
+        public string _HelloMessage
+        {
+            get => _message;
+            set => _message = value;
+        }
+        public Message(double p_hMorning, double p_hMidday, double p_hAfternoon) { 
+            _hMorning = p_hMorning;
+            _hMidday = p_hMidday;
+            _hAfternoon = p_hAfternoon;
+        }
+
+        public void SetMessage()
+        {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            if (DateTime.Now.Hour >= _hMorning && DateTime.Now.Hour < _hMidday && DateTime.Now.DayOfWeek != DayOfWeek.Saturday && DateTime.Now.DayOfWeek != DayOfWeek.Sunday)
+            {
+                _HelloMessage = ("Bonjour " + userName);
+            }
+            else if (DateTime.Now.Hour >= _hMidday && DateTime.Now.Hour < _hAfternoon && DateTime.Now.DayOfWeek != DayOfWeek.Saturday && DateTime.Now.DayOfWeek != DayOfWeek.Sunday)
+            {
+                _HelloMessage = ("Bon après-midi " + userName);
+            }
+            else if ((DateTime.Now.Hour >= _hAfternoon || DateTime.Now.Hour < _hMorning) && (DateTime.Now.DayOfWeek != DayOfWeek.Friday | DateTime.Now.DayOfWeek != DayOfWeek.Saturday || DateTime.Now.DayOfWeek != DayOfWeek.Sunday))
+            {
+                _HelloMessage = ("Bonsoir " + userName);
+            }
+            else if ((DateTime.Now.Hour >= _hAfternoon || DateTime.Now.Hour < _hMorning) && (DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday))
+            {
+                _HelloMessage = ("Bon Week-End " + userName);
+            }
+        }
+    }
+}
---
+++ Helloworld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean running = true;
+            Message message = new Message(9, 13, 18);
+            while (running)
+            {
+                Console.WriteLine("type '' to get message or exit then ENTER");
+                string input = Console.ReadLine();
+                if ( input == "")
+                {
+                    message.SetMessage();
+                    Console.WriteLine(message._HelloMessage);
+                } 
+                else if (input.ToLower().Equals("exit"))
+                {
+                    running = false;
+                }
+            }
+        }
+    }
+}
+
